ab8fb2eedae170648b8eb1ead6ac240ae8204807
chayoosang <yoo7969@naver.com>
2023-09-18 23:55:15 +0900

    [PDW-5] feat: 회의실 선택 틀 생성

---
+++ src/components/officeInfo/OfficeInfo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./OfficeInfo.module.css"
+
+function OfficeInfo() {
+    return (
+        <div>
+            <div>
+                <text>회의실1</text> 
+                <text>401호</text>
+            </div>  {/* 회의실 + 호수 div */}
+            
+            <div>
+                
+            </div>
+
+        </div>
+    );
+}
+
+export default OfficeInfo;
---
+++ src/pages/booking/selectOffice/SelectOffice.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./SelectOffice.module.css";
+
+function SelectOffice() {
+    return (
+        <div>
+            <h2>회의실 예약</h2> {/* 소제목 */}
+        </div> // 전체 화면 - 보라색 화면 끝
+    );
+}
+
+export default SelectOffice;
