cf91e065beb57638b9272368230e5983686fbd04
chayoosang <yoo7969@naver.com>
2023-09-26 16:59:35 +0900

    [PDW-5] feat: 회의실 목록 조회 api 연결

--- src/components/officeInfo/OfficeInfo.js
+++ src/components/officeInfo/OfficeInfo.js
@@ -23,8 +23,8 @@ function OfficeInfo(props) {
     return (
         <Card onClick={props.click}>
             <TitleContainer>
-                <NameText>회의실1</NameText>
-                <CardText>401호</CardText>
+                <NameText>{props.name}</NameText>
+                <CardText>{props.location}</CardText>
             </TitleContainer>
 
             <DetailContainer>
@@ -33,23 +33,17 @@ function OfficeInfo(props) {
                 <InfoContainer>
                     <OfficePeopleContainer>
                         <Capsule color="purple" text="수용인원" />
-                        <CardText>6</CardText>
+                        <CardText>{props.capacity}</CardText>
                     </OfficePeopleContainer>
 
                     <OfficeToolContainer>
-                        <Capsule color="white" text="빔 프로젝터" />
-                        <Capsule color="white" text="마이크" />
-                        <Capsule color="white" text="화상회의" />
-                        <Capsule color="white" text="대형 모니터" />
-                        <Capsule color="white" text="대형 모니터" />
-                        외 2개
+                        {props.facilityList.map((facility) => <Capsule color="white" text={facility}/>)}
                     </OfficeToolContainer>
 
                     <DescriptionContainer>
                         <Capsule color="purple" text="설명" />
                         <OfficeContentText>
-                            이 회의실은 최초로 영국에서 시작되어... 만약 당신이 이 회의실을 사용한다면 행운을 얻게 될 것이고, 이 회의실을 사용하지 않는다면... 각오하셔야 될 것입니다. 이 회의실은 최초로 영국에서 시작되어... 만약 당신이 이 회의실을 사용......
-                            이 회의실은 최초로 영국에서 시작되어... 만약 당신이 이 회의실을 사용한다면 행운을 얻게 될 것이고, 이 회의실을 사용하지 않는다면... 각오하셔야 될 것입니다. 이 회의실은 최초로 영국에서 시작되어... 만약 당신이 이 회의실을 사용......
+                            {props.description}
                         </OfficeContentText>
                     </DescriptionContainer>
 
--- src/pages/booking/selectOffice/SelectOffice.js
+++ src/pages/booking/selectOffice/SelectOffice.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SearchBar from "components/searchBar/SearchBar";
 import OfficeInfo from "components/officeInfo/OfficeInfo";
 import { RightContainer, WhiteContainer, TitleText } from "components/rightContainer/RightContainer";
-
-function click() {
-    alert("1")
-}
+import axios from "axios";
+import { useState } from "react";
 
 function SelectOffice(props) {
+
+    const [offices, setOffices] = useState([]);
+
+    const getOfficeList = () => {
+        axios.get("http://13.124.122.173/offices")
+            .then((Response)=>{setOffices(Response.data.data.content)})
+            .catch((Error)=>{alert(Error)})
+
+        console.log(offices)
+        
+    };
+
+    useEffect(()=> {
+        getOfficeList();
+    }, []);
+
+
     return (
         <RightContainer>
             <TitleText>{props.title}</TitleText>
             <WhiteContainer>
                 <SearchBar />
                 <div className="cardList">
-                    <OfficeInfo click={click} />
-                    <OfficeInfo click={click} />
+                    {offices.map((office) => <OfficeInfo key={office.name} 
+                                                         name={office.name}
+                                                         location={office.location}
+                                                         capacity={office.capacity}
+                                                         facilityList={office.facilityList}
+                                                         description={office.description}
+                                                         />)}
                 </div>
             </WhiteContainer>
         </RightContainer>
