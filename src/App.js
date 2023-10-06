2bb56e139aba78e2457559fb98fab8bd2001dd07
chayoosang <yoo7969@naver.com>
2023-10-06 18:11:01 +0900

    [PDW-39] feat: 회의실 관리 table cell 생성 및 table 구현

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MainPage from 'pages/main/MainPage';
 import OfficeBooking from 'pages/booking/officeBooking/OfficeBooking';
 import SelectResource from 'pages/booking/selectResource/SelectResource';
 import ResourceBooking from 'pages/booking/resourceBooking/ResourceBooking';
+import OfficeManage from 'pages/booking/officeManage/OfficeManage';
 
 function App() {
 
@@ -31,6 +32,9 @@ function App() {
           <Route path='/bookings/resources/:bookingId' element={<ResourceBooking isCheck='true' />} />
           <Route path='/resourceBooking' element={<SelectResource title="자원 예약" />} />
           <Route path='/resourceBooking/:resourceId' element={<ResourceBooking/>} />
+          <Route path='/manage/office' element={<OfficeManage title="회의실 관리"/>} />
+          {/* <Route path='/manage/office:officeId' element={</>} /> */}
+          {/* 회의실 관리 상세 페이지 위에거 사용하면 됩니다. */}
           {/* 나중에 요런식으로 활용하기 */}
           {/* <Route path="/reportManage/:recipeReportIdx" element={<ReportManageDetail />} /> */}
         </Route>
---
+++ src/pages/booking/officeManage/OfficeManageTableCell.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { BookedLineTr } from '../bookedList/BookedList';
+
+
+
+
+function OfficeManageTableCell(props) {
+
+    return (
+        <BookedLineTr>
+            <td width="20%"><Link to={`/manage/office/${props.id}`}>{props.name}</Link></td>
+            <td width="20%">{props.location}</td>
+            <td width="20%">{props.capacity}</td>
+            <td width="40%">{props.description}</td>
+        </BookedLineTr>
+    )
+}
+
+export default OfficeManageTableCell;
