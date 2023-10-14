2581161b2e6659d599b926ea0240b8a0d38ba7e3
psyeon1120 <psycsh@naver.com>
2023-10-14 19:31:36 +0900

    [PDW-44] fix: 직원 관리 링크 제거

--- src/pages/manager/userManage/UserManageLine.js
+++ src/pages/manager/userManage/UserManageLine.js
@@ -7,7 +7,7 @@ function UserManageTableCell(props) {
 
     return (
         <BookedLineTr>
-            <td width="5%"><Link to={`/manage/users/${props.id}`}>{props.name}</Link></td>
+            <td width="5%">{props.name}</td>
             <td width="5%">{props.position}</td>
             <td width="15%">{props.email}</td>
             <td width="10%">{props.department}</td>
