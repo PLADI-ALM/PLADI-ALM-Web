9041256ac3b65aa66ae67dceba895877021bc253
psyeon1120 <psycsh@naver.com>
2023-11-25 02:59:55 +0900

    [PDW-94] feat: 장비 추가 및 수정

--- src/components/rightContainer/RightContainer.js
+++ src/components/rightContainer/RightContainer.js
@@ -15,7 +15,6 @@ export const WhiteContainer = styled.div`
     background: #FFF;
     box-shadow: 0px 4px 14px 0px rgba(0, 0, 0, 0.25);
     overflow-y: scroll;
-    //display: grid;
     height: 100%;
     width: 100%;
     position: relative;
